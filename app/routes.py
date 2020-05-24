5706e2d61018a3f8a6e72a5f04b2928b5a779854
ValentinaLV <tina.lysenok@gmail.com>
2020-05-24 18:42:17 +0300

    added logic for total by date on index

--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,30 @@ from app import app
 from app import db
 from .forms import FoodForm, DateForm
 from .models import Food, LogDate
-from .utils import total_by_food_element
+from .utils import total_by_food_element, total_by_food_date
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    log_dates: list of entry_date in log_date [(datetime.date(2020, 5, 24),),...]
+    log_dates_format: ['May 24, 2020',...]
+    food_by_dates: list of food items by date
+    total_by_dates: list of dicts totals by food item
+    {'date': 'May 24, 2020', 'protein': 0, 'carbohydrates': 0, 'fat': 0, 'calories': 0}
+    """
     form = DateForm()
+    dates = LogDate.query.all()
 
     log_dates = db.session.query(LogDate.entry_date) \
         .order_by(desc(LogDate.entry_date)) \
         .all()
     log_dates_format = [datetime.strftime(date[0], '%B %d, %Y') for date in log_dates]
 
+    total_by_dates = [total_by_food_date(date.food, date.entry_date.strftime('%B %d, %Y'))
+                      for date in dates]
+
+    print(total_by_dates)
     if request.method == 'POST':
         date = request.form['date']
         log_date = LogDate(entry_date=date)
@@ -26,14 +39,16 @@ def index():
         return redirect(url_for('index'))
     return render_template('base.html',
                            form=form,
-                           dates=log_dates_format)
+                           dates=log_dates_format,
+                           total_by_dates=total_by_dates)
 
 
-@app.route('/total-by-day/<date>', methods=['GET', 'POST'],
-           defaults={'date': datetime.today().strftime('%B %d, %Y')})
+@app.route('/total-by-day/<date>', methods=['GET', 'POST'])
 def total_by_day(date):
     food_results = Food.query.all()
-    log_date = LogDate.query.filter(LogDate.entry_date == date).first()
+    log_date = LogDate.query. \
+        filter(LogDate.entry_date == date) \
+        .first()
 
     if log_date:
         log_date_format = log_date.entry_date.strftime('%B %d, %Y')
@@ -47,7 +62,6 @@ def total_by_day(date):
         db.session.commit()
 
     food_by_date = log_date.food
-    print(food_by_date)
     total = total_by_food_element(food_by_date)
     return render_template('day.html',
                            date=log_date_format,
--- app/utils.py
+++ app/utils.py
@@ -11,3 +11,19 @@ def total_by_food_element(food):
         total['fat'] += item.fat
         total['calories'] += item.calories
     return total
+
+
+def total_by_food_date(food, date):
+    total = {
+        'date': date,
+        'protein': 0,
+        'carbohydrates': 0,
+        'fat': 0,
+        'calories': 0
+    }
+    for item in food:
+        total['protein'] += item.protein
+        total['carbohydrates'] += item.carbohydrates
+        total['fat'] += item.fat
+        total['calories'] += item.calories
+    return total
